ba7f6ccd8a9a27dc16931776651361458c0e5ce5
Atia-Khan <atia.khan@xloopdigital.com>
2023-05-07 20:43:15 +0500

    First Test Case Passed

--- src/main/java/VolcanoAnalyzer.java
+++ src/main/java/VolcanoAnalyzer.java
@@ -5,6 +5,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
@@ -30,5 +32,11 @@ public class VolcanoAnalyzer {
     }
 
     //add methods here to meet the requirements in README.md
+
+      public List<Volcano> eruptedInEighties() {
+        return volcanos.stream()
+            .filter(v -> v.getYear() >= 1980 && v.getYear() <= 1989)
+            .collect(Collectors.toList());
+    }
 
 }
